0de1418c0fce158772b2292d9adea70872dba5d7
abc-d <abc-d@DESKTOP-RHA7QSB>
2024-04-21 04:33:46 +0300

    #Api program.cs'e unitofwork eklendi

--- ProductApi.API/Program.cs
+++ ProductApi.API/Program.cs
@@ -1,3 +1,6 @@
+using ProductApi.DataAccess.Abstract;
+using ProductApi.DataAccess.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 
 var app = builder.Build();
 
